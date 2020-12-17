7e6893ad5ad72aa442a49dbe0abe3df6321b30d7
despc <idespc@gmail.com>
2020-12-17 21:57:23 +0300

    Initial commit

---
+++ FixTurrets/FixTurretsPatch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using NLog;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Weapons;
+using Torch.Managers.PatchManager;
+using Torch.Utils;
+using VRage.Game;
+using VRage.Game.Components;
+
+namespace FixTurrets
+{
+    [PatchShim]
+    public static class FixTurretsPatch
+    {
+        
+        [ReflectedMethod(Name = "RotateModels")]
+        private static Action<MyLargeTurretBase> RotateModels;
+        
+
+
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public static void Patch(PatchContext ctx)
+        {
+            Log.Info("Patch init");
+
+            var methodInfos = typeof(MyLargeTurretBase).GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            var initMethodInfo = methodInfos.Where(m => m.Name.Equals("Init")).ToArray()[0];
+            ctx.GetPattern(initMethodInfo).Prefixes.Add(
+                typeof(FixTurretsPatch).GetMethod(nameof(TurretInitPatch),
+                    BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
+        }
+        
+        
+        private static void TurretInitPatch(MyLargeTurretBase __instance, MyObjectBuilder_CubeBlock objectBuilder, MyCubeGrid cubeGrid)
+        {
+            __instance.PositionComp.OnPositionChanged += pos =>
+                OnPositionChanged(pos, __instance);
+        }
+        
+        private static void OnPositionChanged(MyPositionComponentBase myPositionComponentBase, MyLargeTurretBase __instance)
+        {
+            RotateModels.Invoke(__instance);
+        }
+    }
+}
---
+++ FixTurrets/FixTurretsPlugin.cs
@@ -0,0 +1,16 @@
+﻿using NLog;
+using Torch;
+using Torch.API;
+
+namespace FixTurrets
+{
+    public class FixTurretsPlugin : TorchPluginBase
+    {
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public override void Init(ITorchBase torch)
+        {
+            Log.Info("Init FixTurretsPlugin");
+        }
+    }
+}
