dd94fe873259fc490a777f249ed78a54069e2b52
despc <idespc@gmail.com>
2021-05-08 14:08:31 +0300

    rename + projector patch WIP

---
+++ SentisOptimisations/TurretPatchs.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using NLog;
+using Sandbox;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Weapons;
+using Torch.Managers.PatchManager;
+using Torch.Utils;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.Entity;
+
+namespace FixTurrets
+{
+    [PatchShim]
+    public static class FixTurretsPatch
+    {
+
+        [ReflectedMethod(Name = "RotateModels")]
+        private static Action<MyLargeTurretBase> RotateModels;
+        
+        [ReflectedGetter(Name = "m_entitiesForUpdate10")]
+        private static Func<MyParallelEntityUpdateOrchestrator, MyDistributedUpdater<List<MyEntity>, MyEntity>> m_entitiesForUpdate10;
+
+
+
+        private static void OnPositionChanged(MyPositionComponentBase myPositionComponentBase,
+            MyLargeTurretBase __instance)
+        {
+            if ((ulong) __instance.EntityId % 100 == MySandboxGame.Static.SimulationFrameCounter % 100)
+            {
+                RotateModels.Invoke(__instance);
+            }
+        }
+
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public static void Patch(PatchContext ctx)
+        {
+            Log.Info("Patch init");
+            //
+            var methodInfos = typeof(MyLargeTurretBase).GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            var initMethodInfo = methodInfos.Where(m => m.Name.Equals("Init")).ToArray()[0];
+            ctx.GetPattern(initMethodInfo).Prefixes.Add(
+                typeof(FixTurretsPatch).GetMethod(nameof(TurretInitPatch),
+                    BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
+
+
+            var methodInfo = typeof(MyLargeTurretBase).GetMethod("CheckAndSelectNearTargetsParallel",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+
+            ctx.GetPattern(methodInfo).Prefixes.Add(
+                typeof(FixTurretsPatch).GetMethod(nameof(DisableCheckAndSelectNearTargetsParallel),
+                    BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
+        }
+
+
+        private static void TurretInitPatch(MyLargeTurretBase __instance, MyObjectBuilder_CubeBlock objectBuilder,
+            MyCubeGrid cubeGrid)
+        {
+            __instance.PositionComp.OnPositionChanged += pos =>
+                OnPositionChanged(pos, __instance);
+        }
+
+        private static bool DisableCheckAndSelectNearTargetsParallel(MyLargeTurretBase __instance)
+        {
+            if (MySandboxGame.Static.SimulationFrameCounter % 3 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        
+
+    }
+}
