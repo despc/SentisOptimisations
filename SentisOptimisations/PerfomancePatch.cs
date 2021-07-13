ef4a535a0cdcac4317615edda254de0a99dff9e9
despc <idespc@gmail.com>
2021-07-13 19:11:20 +0300

    fix welders optimisation & ThrustDamageAsync

--- SentisOptimisations/PerfomancePatch.cs
+++ SentisOptimisations/PerfomancePatch.cs
@@ -7,10 +7,12 @@ using NLog;
 using Sandbox;
 using Sandbox.Engine.Physics;
 using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Blocks;
 using Sandbox.Game.Weapons;
 using SpaceEngineers.Game.Entities.Blocks;
 using Torch.Managers.PatchManager;
 using VRage.ModAPI;
+using VRageMath;
 
 namespace SentisOptimisationsPlugin
 {
@@ -19,8 +21,9 @@ namespace SentisOptimisationsPlugin
     {
         public static readonly Logger Log = LogManager.GetCurrentClassLogger();
         public static Dictionary<long, long> entityesInSZ = new Dictionary<long, long>();
-        public static Dictionary<long, MyWelder.ProjectionRaycastData[]> welderCache = new Dictionary<long, MyWelder.ProjectionRaycastData[]>();
+            //public static Dictionary<long, MyWelder.ProjectionRaycastData[]> welderCache = new Dictionary<long, MyWelder.ProjectionRaycastData[]>();
         public static Dictionary<long, long> welderCounter = new Dictionary<long, long>();
+        public static Dictionary<long, long> thrustCounter = new Dictionary<long, long>();
 
         public static void Patch(PatchContext ctx)
         {
@@ -29,6 +32,14 @@ namespace SentisOptimisationsPlugin
 
             ctx.GetPattern(MethodPhantom_Leave).Prefixes.Add(
                 typeof(PerfomancePatch).GetMethod(nameof(MethodPhantom_LeavePatched),
+                    BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
+            
+            
+            var MethodThrustDamageAsync = typeof(MyThrust).GetMethod
+                ("ThrustDamageAsync", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            ctx.GetPattern(MethodThrustDamageAsync).Prefixes.Add(
+                typeof(PerfomancePatch).GetMethod(nameof(MethodThrustDamageAsyncPatched),
                     BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
 
             // Кажется это уменьшает тягу)
@@ -47,11 +58,18 @@ namespace SentisOptimisationsPlugin
                 typeof(PerfomancePatch).GetMethod(nameof(MethodFindProjectedBlocksPatched),
                     BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
             
-            ctx.GetPattern(MethodFindProjectedBlocks).Suffixes.Add(
-                typeof(PerfomancePatch).GetMethod(nameof(MethodFindProjectedBlocksPatchedAddCache),
-                    BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
+            //ctx.GetPattern(MethodFindProjectedBlocks).Suffixes.Add(
+            //    typeof(PerfomancePatch).GetMethod(nameof(MethodFindProjectedBlocksPatchedAddCache),
+            //        BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
             
             
+            
+            //var MethodBuildInternal = typeof(MyProjectorBase).GetMethod
+            //    ("BuildInternal", BindingFlags.Instance | BindingFlags.NonPublic);
+            //
+            //ctx.GetPattern(MethodBuildInternal).Prefixes.Add(
+            //    typeof(PerfomancePatch).GetMethod(nameof(MethodBuildInternalPatched),
+            //        BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
             //var MethodMyReplicationServerDestroy = typeof(MyReplicationServer).GetMethod
             //    (nameof(MyReplicationServer.Destroy), BindingFlags.Instance | BindingFlags.Public);
             //
@@ -112,31 +130,59 @@ namespace SentisOptimisationsPlugin
             {
                 if (welderCounter[__instance.EntityId] < 5)
                 {
-                    if (welderCache.ContainsKey(__instance.EntityId))
-                    {
-                        __result = welderCache[__instance.EntityId];
+                    //if (welderCache.ContainsKey(__instance.EntityId))
+                    //{
+                        __result = new MyWelder.ProjectionRaycastData[0];
                         welderCounter[__instance.EntityId] = welderCounter[__instance.EntityId] + 1; 
-                        return false;
-                    }
+                    //    return false;
+                    //}
                 }
             }
             welderCounter[__instance.EntityId] = 0; 
             return true;
         }
-        
-        private static void MethodFindProjectedBlocksPatchedAddCache(MyShipWelder __instance, ref MyWelder.ProjectionRaycastData[] __result)
+
+
+        private static bool MethodBuildInternalPatched(MyProjectorBase __instance,
+            Vector3I cubeBlockPosition,
+            long owner,
+            long builder,
+            bool requestInstant = true,
+            long builtBy = 0)
         {
-            welderCache[__instance.EntityId] = __result;
-        }
-        
-        private static bool MethodUpdateThrustsPatched()
-        {
-            
-            if (MySandboxGame.Static.SimulationFrameCounter % 3 == 0)
+            try
             {
-                return true;
+                InvokeInstanceMethod(typeof(MyProjectorBase), __instance, "BuildInternal",
+                    new object[] {cubeBlockPosition, owner, builder, requestInstant, builtBy});
+            }
+            catch (Exception e)
+            {
+                //welderCache.Clear();
+                welderCounter.Clear();
+                Log.Error(e);
             }
             return false;
+    }
+        
+        private static void MethodFindProjectedBlocksPatchedAddCache(MyShipWelder __instance,
+            ref MyWelder.ProjectionRaycastData[] __result)
+        {
+            //welderCache[__instance.EntityId] = __result;
+        }
+
+        private static bool MethodThrustDamageAsyncPatched(MyThrust __instance)
+        {
+            if (thrustCounter.ContainsKey(__instance.EntityId))
+            {
+                if (thrustCounter[__instance.EntityId] < 5)
+                {
+                    thrustCounter[__instance.EntityId] = thrustCounter[__instance.EntityId] + 1;
+                    return false;
+                }
+            }
+
+            thrustCounter[__instance.EntityId] = 0;
+            return true;
         }
 
         private static object InvokeInstanceMethod(Type type, object instance, string methodName, Object[] args)
