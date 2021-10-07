5da9a187748ed74c458a18ffdd88cf638f252fa3
despc <idespc@gmail.com>
2021-10-07 19:07:14 +0300

    assembler fix

---
+++ SentisOptimisations/Clusters/AssemblersConcurrencyFix.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using NLog;
+using Sandbox.Game.Entities.Cube;
+using Sandbox.Game.GameSystems;
+using Sandbox.Game.GameSystems.Conveyors;
+using SentisOptimisations;
+using Torch.Managers.PatchManager;
+using VRage.Utils;
+
+namespace FixTurrets.Clusters
+{
+    [PatchShim]
+    public static class AssemblersConcurrencyFix
+    {
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public static void Patch(PatchContext ctx)
+        {
+            var MethodGetMasterAssembler = typeof(MyAssembler).GetMethod
+                ("GetMasterAssembler", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            ctx.GetPattern(MethodGetMasterAssembler).Prefixes.Add(
+                typeof(AssemblersConcurrencyFix).GetMethod(nameof(GetMasterAssemblerPatched),
+                    BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
+        }
+
+        private static bool GetMasterAssemblerPatched(MyAssembler __instance, ref MyAssembler __result)
+        {
+            try
+            {
+                List<IMyConveyorEndpoint> m_conveyorEndpoints = new List<IMyConveyorEndpoint>();
+
+                Predicate<IMyConveyorEndpoint> m_vertexPredicate = new Predicate<IMyConveyorEndpoint>(
+                    vertex => vertex.CubeBlock is MyAssembler && vertex.CubeBlock != __instance);
+                Predicate<IMyConveyorEndpoint> m_edgePredicate = new Predicate<IMyConveyorEndpoint>(edge =>
+                    edge.CubeBlock.OwnerId == 0L || __instance.FriendlyWithBlock(edge.CubeBlock));
+                MyGridConveyorSystem.FindReachable(__instance.ConveyorEndpoint, m_conveyorEndpoints, m_vertexPredicate,
+                    m_edgePredicate);
+                m_conveyorEndpoints.ShuffleList<IMyConveyorEndpoint>();
+                foreach (IMyConveyorEndpoint conveyorEndpoint in m_conveyorEndpoints)
+                {
+                    if (conveyorEndpoint.CubeBlock is MyAssembler cubeBlock && !cubeBlock.DisassembleEnabled &&
+                        !cubeBlock.IsSlave)
+                    {
+                        List<MyProductionBlock.QueueItem> m_queue =
+                            (List<MyProductionBlock.QueueItem>) ReflectionUtils.GetInstanceField(typeof(MyAssembler),
+                                cubeBlock, "m_queue");
+                        if (m_queue.Count > 0)
+                        {
+                            __result = cubeBlock;
+                            return true;
+                        }
+                    }
+                }
+
+                __result = null;
+            }
+            catch (Exception e)
+            {
+                Log.Error(e);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
