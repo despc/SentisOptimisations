b43bf1c6616fe4e239b0b6952b2e3db4a6827e92
despc <idespc@gmail.com>
2022-03-27 15:35:47 +0300

    tier price

--- SentisOptimisations/NpcStorePatch.cs
+++ SentisOptimisations/NpcStorePatch.cs
@@ -28,7 +28,78 @@ namespace SentisOptimisationsPlugin
             {
                 if (itemId.SubtypeName.Equals("ZoneChip"))
                 {
-                    minimalPrice = 90000;
+                    minimalPrice = 220000;
+                }
+                if (itemId.SubtypeName.Equals("Structure_t2"))
+                {
+                    minimalPrice = 2000000;
+                }
+                if (itemId.SubtypeName.Equals("Structure_t3"))
+                {
+                    minimalPrice = 5000000;
+                }
+                if (itemId.SubtypeName.Equals("Production_t2"))
+                {
+                    minimalPrice = 2000000;
+                }
+                if (itemId.SubtypeName.Equals("Production_t3"))
+                {
+                    minimalPrice = 5000000;
+                }
+                if (itemId.SubtypeName.Equals("Turret_t2"))
+                {
+                    minimalPrice = 2000000;
+                }
+                if (itemId.SubtypeName.Equals("Turret_t3"))
+                {
+                    minimalPrice = 5000000;
+                }
+                if (itemId.SubtypeName.Equals("PowerCell_t2"))
+                {
+                    minimalPrice = 300000;
+                }
+                if (itemId.SubtypeName.Equals("PowerCell_t3"))
+                {
+                    minimalPrice = 1000000;
+                }
+                if (itemId.SubtypeName.Equals("SolarCell_t2"))
+                {
+                    minimalPrice = 100000;
+                }
+                if (itemId.SubtypeName.Equals("SolarCell_t3"))
+                {
+                    minimalPrice = 200000;
+                }
+                if (itemId.SubtypeName.Equals("Thrust_t2"))
+                {
+                    minimalPrice = 500000;
+                }
+                if (itemId.SubtypeName.Equals("Thrust_t3"))
+                {
+                    minimalPrice = 2000000;
+                }
+                
+                
+                if (itemId.SubtypeName.Equals("Titanium"))
+                {
+                    if (itemId.TypeId.ToString().Contains("Ingot"))
+                        minimalPrice = 500000;
+                    else
+                        minimalPrice = 50000;
+                }
+                if (itemId.SubtypeName.Equals("Copper"))
+                {
+                    if (itemId.TypeId.ToString().Contains("Ingot"))
+                        minimalPrice = 400000;
+                    else
+                        minimalPrice = 40000;
+                }
+                if (itemId.SubtypeName.Equals("Zinc"))
+                {
+                    if (itemId.TypeId.ToString().Contains("Ingot"))
+                        minimalPrice = 600000;
+                    else
+                        minimalPrice = 60000;
                 }
             }
             catch (Exception e)
