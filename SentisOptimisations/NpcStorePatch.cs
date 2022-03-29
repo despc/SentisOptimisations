1c579237daf713ce8c5ad8e83bcb4d250ca04c5b
despc <idespc@gmail.com>
2022-03-30 00:52:45 +0300

    NPC Store

--- SentisOptimisations/NpcStorePatch.cs
+++ SentisOptimisations/NpcStorePatch.cs
@@ -1,17 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Windows.Documents;
 using NLog;
+using Sandbox.Game.Entities.Blocks;
+using Sandbox.Game.Multiplayer;
 using Sandbox.Game.World.Generator;
 using Torch.Managers.PatchManager;
 using VRage.Game;
+using VRage.Game.ObjectBuilders.Definitions;
 
 namespace SentisOptimisationsPlugin
 {
     [PatchShim]
     public static class NpcStorePatch
     {
-        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
+            
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        
+        public static string[] prohibitedStoreItems = new string[] {
+            "Structure_t2",
+            "Structure_t3",
+            "Production_t2",
+            "Production_t3",
+            "Turret_t2",
+            "Turret_t3",
+            "PowerCell_t2",
+            "PowerCell_t3",
+            "SolarCell_t2",
+            "SolarCell_t3",
+            "Thrust_t2",
+            "Thrust_t3",
+            "Titanium",
+            "Copper",
+            "Zinc",
+            "TradePack_t1",
+            "TradePack_t2",
+            "TradePack_t3",
+            "Tech_Structure",
+            "Tech_Turret",
+            "Tech_Production",
+            "Tech_Solar",
+            "Tech_Power",
+            "Tech_Ammo"};
+        
         public static void Patch(PatchContext ctx)
         {
             var MethodGetItemMinimalPrice = typeof(MyMinimalPriceCalculator).GetMethod(
@@ -20,9 +53,58 @@ namespace SentisOptimisationsPlugin
             ctx.GetPattern(MethodGetItemMinimalPrice).Suffixes.Add(
                 typeof(NpcStorePatch).GetMethod(nameof(PatchGetItemMinimalPrice),
                     BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
+            
+            
+            // Sandbox.Game.Multiplayer.
+                
+            var MethodInit = typeof(MyFactionCollection).GetMethod(
+                nameof(MyFactionCollection.Init), BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
+            //
+            ctx.GetPattern(MethodInit).Prefixes.Add(
+                typeof(NpcStorePatch).GetMethod(nameof(MethodInitPatch),
+                    BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
         }
 
-        private static void PatchGetItemMinimalPrice(MyDefinitionId itemId, ref int minimalPrice)
+
+        private static void MethodInitPatch(MyFactionCollection __instance, MyObjectBuilder_FactionCollection builder)
+        {
+            try
+            {
+                foreach (var myObjectBuilderFaction in builder.Factions)
+                {
+                    foreach (var myObjectBuilderStation in myObjectBuilderFaction.Stations)
+                    {
+                        foreach (var myObjectBuilderStoreItem in new List<MyObjectBuilder_StoreItem>(myObjectBuilderStation.StoreItems))
+                        {
+                            if (isProhibited(myObjectBuilderStoreItem))
+                            {
+                                myObjectBuilderStation.StoreItems.Remove(myObjectBuilderStoreItem);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error(e);
+            }
+
+        }
+
+        private static bool isProhibited(MyObjectBuilder_StoreItem myObjectBuilderStoreItem)
+        {
+            if (myObjectBuilderStoreItem.ItemType == ItemTypes.PhysicalItem)
+            {
+                if (prohibitedStoreItems.Contains(myObjectBuilderStoreItem.Item.Value.SubtypeId))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static void PatchGetItemMinimalPrice(MyDefinitionId itemId, ref int minimalPrice, ref bool __result)
         {
             try
             {
@@ -30,76 +112,9 @@ namespace SentisOptimisationsPlugin
                 {
                     minimalPrice = 220000;
                 }
-                if (itemId.SubtypeName.Equals("Structure_t2"))
+                if (prohibitedStoreItems.Contains(itemId.SubtypeName))
                 {
-                    minimalPrice = 2000000;
-                }
-                if (itemId.SubtypeName.Equals("Structure_t3"))
-                {
-                    minimalPrice = 5000000;
-                }
-                if (itemId.SubtypeName.Equals("Production_t2"))
-                {
-                    minimalPrice = 2000000;
-                }
-                if (itemId.SubtypeName.Equals("Production_t3"))
-                {
-                    minimalPrice = 5000000;
-                }
-                if (itemId.SubtypeName.Equals("Turret_t2"))
-                {
-                    minimalPrice = 2000000;
-                }
-                if (itemId.SubtypeName.Equals("Turret_t3"))
-                {
-                    minimalPrice = 5000000;
-                }
-                if (itemId.SubtypeName.Equals("PowerCell_t2"))
-                {
-                    minimalPrice = 300000;
-                }
-                if (itemId.SubtypeName.Equals("PowerCell_t3"))
-                {
-                    minimalPrice = 1000000;
-                }
-                if (itemId.SubtypeName.Equals("SolarCell_t2"))
-                {
-                    minimalPrice = 100000;
-                }
-                if (itemId.SubtypeName.Equals("SolarCell_t3"))
-                {
-                    minimalPrice = 200000;
-                }
-                if (itemId.SubtypeName.Equals("Thrust_t2"))
-                {
-                    minimalPrice = 500000;
-                }
-                if (itemId.SubtypeName.Equals("Thrust_t3"))
-                {
-                    minimalPrice = 2000000;
-                }
-                
-                
-                if (itemId.SubtypeName.Equals("Titanium"))
-                {
-                    if (itemId.TypeId.ToString().Contains("Ingot"))
-                        minimalPrice = 500000;
-                    else
-                        minimalPrice = 50000;
-                }
-                if (itemId.SubtypeName.Equals("Copper"))
-                {
-                    if (itemId.TypeId.ToString().Contains("Ingot"))
-                        minimalPrice = 400000;
-                    else
-                        minimalPrice = 40000;
-                }
-                if (itemId.SubtypeName.Equals("Zinc"))
-                {
-                    if (itemId.TypeId.ToString().Contains("Ingot"))
-                        minimalPrice = 600000;
-                    else
-                        minimalPrice = 60000;
+                    __result = false;
                 }
             }
             catch (Exception e)
