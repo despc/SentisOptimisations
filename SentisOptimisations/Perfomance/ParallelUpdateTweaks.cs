a8d85182afd10708c1847a99a14a4087f1c06129
despc <idespc@gmail.com>
2023-03-24 09:43:09 +0300

    skip static thrusters update

---
+++ SentisOptimisations/Perfomance/ParallelUpdateTweaks.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using NLog;
+using Sandbox.Game.Entities;
+using Torch.Managers.PatchManager;
+
+namespace FixTurrets.Perfomance
+{
+    [PatchShim]
+    public class ParallelUpdateTweaks
+    {
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        private static Dictionary<long, int> ResourceDistributorCounters = new Dictionary<long, int>();
+
+        private static Type MyThrusterBlockThrustComponentType =
+            typeof(MyParallelEntityUpdateOrchestrator).Assembly.GetType(
+                "Sandbox.Game.EntityComponents.MyThrusterBlockThrustComponent");
+
+        private static MethodInfo GetEntityMethod = MyThrusterBlockThrustComponentType.GetProperty("Entity",
+            BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly).GetMethod;
+
+        public static void Patch(PatchContext ctx)
+        {
+            var MethodThrustUpdateBeforeSimulation = MyThrusterBlockThrustComponentType.GetMethod
+                ("UpdateBeforeSimulation", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
+            ctx.GetPattern(MethodThrustUpdateBeforeSimulation).Prefixes.Add(
+                typeof(ParallelUpdateTweaks).GetMethod(nameof(MethodThrustUpdateBeforeSimulationPatched),
+                    BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
+        }
+
+        private static bool MethodThrustUpdateBeforeSimulationPatched(Object __instance)
+        {
+            MyCubeGrid grid = (MyCubeGrid)GetEntityMethod.Invoke(__instance, new object[] { });
+
+            if (grid.IsStatic)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
