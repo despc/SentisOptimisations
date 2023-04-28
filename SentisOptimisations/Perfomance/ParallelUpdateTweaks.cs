514b3205e06cee98db826a609d5f3d6398e20523
despc <idespc@gmail.com>
2023-04-28 14:12:37 +0300

    disable static thrust + MyEntity3DSoundEmitter

--- SentisOptimisations/Perfomance/ParallelUpdateTweaks.cs
+++ SentisOptimisations/Perfomance/ParallelUpdateTweaks.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Reflection;
 using NLog;
+using Sandbox.Game.Entities;
 using Sandbox.Game.Entities.Character;
 using Torch.Managers.PatchManager;
 
@@ -12,8 +13,30 @@ namespace FixTurrets.Perfomance
         public static readonly Logger Log = LogManager.GetCurrentClassLogger();
         public static readonly Random r = new Random();
 
+        private static Type MyThrusterBlockThrustComponentType =
+            typeof(MyParallelEntityUpdateOrchestrator).Assembly.GetType(
+                "Sandbox.Game.EntityComponents.MyThrusterBlockThrustComponent");
+        
+        private static MethodInfo GetEntityMethod = MyThrusterBlockThrustComponentType.GetProperty("Entity",
+            BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly).GetMethod;
+        
         public static void Patch(PatchContext ctx)
         {
+            
+            var MethodThrustUpdateBeforeSimulation = MyThrusterBlockThrustComponentType.GetMethod
+                ("UpdateBeforeSimulation", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
+            ctx.GetPattern(MethodThrustUpdateBeforeSimulation).Prefixes.Add(
+                typeof(ParallelUpdateTweaks).GetMethod(nameof(MethodThrustUpdateBeforeSimulationPatched),
+                    BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
+            
+            var MethodSoundEmitterUpdate = typeof(MyEntity3DSoundEmitter).GetMethod
+                (nameof(MyEntity3DSoundEmitter.Update), BindingFlags.Instance | BindingFlags.Public);
+
+            ctx.GetPattern(MethodSoundEmitterUpdate).Prefixes.Add(
+                typeof(ParallelUpdateTweaks).GetMethod(nameof(Disabled),
+                    BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
+
+            
             var MethodUpdateHeadAndWeapon = typeof(MyCharacter).GetMethod
                 ("UpdateHeadAndWeapon", BindingFlags.Instance | BindingFlags.NonPublic);
 
@@ -22,6 +45,26 @@ namespace FixTurrets.Perfomance
                     BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic));
         }
 
+        private static bool MethodThrustUpdateBeforeSimulationPatched(Object __instance)
+        {
+            try
+            {
+                MyCubeGrid grid = (MyCubeGrid)GetEntityMethod.Invoke(__instance, new object[] { });
+
+                if (grid == null || grid.IsStatic)
+                {
+                    return false;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Log.Error("ThrustUpdate exception ", e);
+            }
+            
+            return true;
+        }
+        
         private static bool Disabled()
         {
             return false;
